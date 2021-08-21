2373fad5a309ae711a1514d2823a3d9bf212e56a
ashrithyvs <ashrithy20@gmail.com>
2021-08-21 13:36:46 +0530

    Added intro-btns

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -17,9 +17,18 @@ function Intro() {
       className="intro-section d-flex flex-column justify-content-end"
       style={background}
     >
-      <Button className=" join-btn align-self-end" variant="outline-light">
-        Join Group
-      </Button>
+      <div className="intro-btn">
+        <Button
+          className="align-self-start"
+          style={{ border: "0px" }}
+          variant="outline-light"
+        >
+          <i class="fas fa-arrow-left"></i>
+        </Button>
+        <Button className="align-self-end" variant="outline-light">
+          Join Group
+        </Button>
+      </div>
       <Container className="text-white p-5">
         <h3>Computer Engineering</h3>
         <span>142,765 Computer Engineers follow this</span>
